1aab2fd3e9b64dba7fd484264aa4a34279ebe4d5
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-01-17 11:29:06 -0500

    initial commit

---
+++ bubbleSort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void printValues(int*){
+    
+}
+void sort(int*){
+
+}
+void swap(int*, int*){
+
+}
+
+int main(){
+    int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+    printf("Before: \n");
+    printValues(values);
+    
+    // test swap
+   e
+}
